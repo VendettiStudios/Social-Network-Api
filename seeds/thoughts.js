5a6903d0537356dee0f380debbd3e8294a81e48d
DH <daniel@vendettistudios.com>
2022-09-27 17:38:16 -0700

    user seeds, index.js routing

---
+++ seeds/thoughts.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        thoughtText: 'Thinking about coding',
+        username: 'devGuy',
+        reactions: [
+            {
+                reactionBody: 'Coding!?',
+                username: 'cookingGuy',
+            },
+            {
+                reactionBody: 'not fo me',
+                username: 'footballGuy',
+            },
+        ],
+    },
+    {
+        thoughtText: 'This is a thought about cooking.',
+        username: 'cookingGuy',
+        reactions: [
+            {
+                reactionBody: 'I am Hangry!!',
+                username: 'devGuy',
+            },
+            {
+                reactionBody: 'Super Hangry!!',
+                username: 'footballGuy',
+            },
+        ],
+    },
+    {
+        thoughtText: 'This is a thought about football.',
+        username: 'footballGuy',
+        reactions: [
+            {
+                reactionBody: 'I can vibe with fantasy football',
+                username: 'devGuy',
+            },
+            {
+                reactionBody: 'I watch it while i cook!!',
+                username: 'cookingGuy',
+            },
+        ],
+    },
+];
---
+++ seeds/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        username: 'devGuy',
+        email: 'devGuy@dev.com',
+    },
+    {
+        username: 'footballGuy',
+        email: 'footballGuy@jock.com',
+    },
+    {
+        username: 'cookingGuy',
+        email: 'cookingGuy@cheffin.com',
+    },
+]
