e5aa8429e1bc170f83f352c2013a0aa6aa8d98e7
DH <daniel@vendettistudios.com>
2022-09-28 22:25:44 -0700

    thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,16 +8,18 @@ const {
   removeReaction,
 } = require('../../controllers/thoughtController');
 
-// /api/students
+// /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/students/:studentId
+// /api/thoughts/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
 
-// /api/students/:studentId/assignments
+router.route('/:thoughtId').put(updateThought);
+
+// /api/thoughts/:thoughtId/reactions
 router.route('/:thoughtId/reactions').post(addReaction);
 
-// /api/students/:studentId/assignments/:assignmentId
+// /api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
